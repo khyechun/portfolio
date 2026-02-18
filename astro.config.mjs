7612ed039d0d0b58b85a86aa5e3d56eb7d684716
Khye Chun Liew <khyechun@botmd.io>
2026-02-18 23:15:32 +0800

    Add github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   integrations: [],
+  site: "https://khyechun.github.io",
+  base: "/master",
   devToolbar: {
     enabled: false,
   },
